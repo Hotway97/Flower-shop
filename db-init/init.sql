a5ab99bbd7a85dcafc1f29c46aea79059040c518
Hotway97 <Hotway97@yandex.ru>
2025-05-01 13:27:17 +0300

    fix init sql

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,10 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 17.4
--- Dumped by pg_dump version 17.4 (Homebrew)
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -17,29 +10,11 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
---
-
 CREATE SCHEMA IF NOT EXISTS public;
-
-
 ALTER SCHEMA public OWNER TO pg_database_owner;
 
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-
 SET default_tablespace = '';
-
 SET default_table_access_method = heap;
-
---
--- Name: cart_item; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.cart_item (
                                   id bigint NOT NULL,
@@ -47,13 +22,7 @@ CREATE TABLE public.cart_item (
                                   cart_id bigint NOT NULL,
                                   product_id bigint NOT NULL
 );
-
-
 ALTER TABLE public.cart_item OWNER TO postgres;
-
---
--- Name: cart_item_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.cart_item_id_seq
     START WITH 1
@@ -61,33 +30,15 @@ CREATE SEQUENCE public.cart_item_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.cart_item_id_seq OWNER TO postgres;
-
---
--- Name: cart_item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.cart_item_id_seq OWNED BY public.cart_item.id;
-
-
---
--- Name: carts; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.carts (
                               id bigint NOT NULL,
                               total_price bigint NOT NULL,
                               user_id bigint
 );
-
-
 ALTER TABLE public.carts OWNER TO postgres;
-
---
--- Name: carts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.carts_id_seq
     START WITH 1
@@ -95,20 +46,8 @@ CREATE SEQUENCE public.carts_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.carts_id_seq OWNER TO postgres;
-
---
--- Name: carts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.carts_id_seq OWNED BY public.carts.id;
-
-
---
--- Name: chat; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.chat (
                              chat_id bigint NOT NULL,
@@ -116,13 +55,7 @@ CREATE TABLE public.chat (
                              created_at timestamp(6) without time zone NOT NULL,
                              user_id bigint
 );
-
-
 ALTER TABLE public.chat OWNER TO postgres;
-
---
--- Name: chat_chat_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.chat_chat_id_seq
     START WITH 1
@@ -130,20 +63,8 @@ CREATE SEQUENCE public.chat_chat_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.chat_chat_id_seq OWNER TO postgres;
-
---
--- Name: chat_chat_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.chat_chat_id_seq OWNED BY public.chat.chat_id;
-
-
---
--- Name: images; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.images (
                                id bigint NOT NULL,
@@ -155,13 +76,7 @@ CREATE TABLE public.images (
                                size bigint,
                                product_id bigint
 );
-
-
 ALTER TABLE public.images OWNER TO postgres;
-
---
--- Name: images_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.images_id_seq
     START WITH 1
@@ -169,20 +84,8 @@ CREATE SEQUENCE public.images_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.images_id_seq OWNER TO postgres;
-
---
--- Name: images_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.images_id_seq OWNED BY public.images.id;
-
-
---
--- Name: message; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.message (
                                 message_id bigint NOT NULL,
@@ -192,13 +95,7 @@ CREATE TABLE public.message (
                                 chat_id bigint NOT NULL,
                                 user_id bigint
 );
-
-
 ALTER TABLE public.message OWNER TO postgres;
-
---
--- Name: message_message_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.message_message_id_seq
     START WITH 1
@@ -206,20 +103,8 @@ CREATE SEQUENCE public.message_message_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.message_message_id_seq OWNER TO postgres;
-
---
--- Name: message_message_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.message_message_id_seq OWNED BY public.message.message_id;
-
-
---
--- Name: order_product; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.order_product (
                                       id bigint NOT NULL,
@@ -227,13 +112,7 @@ CREATE TABLE public.order_product (
                                       order_id bigint NOT NULL,
                                       product_id bigint NOT NULL
 );
-
-
 ALTER TABLE public.order_product OWNER TO postgres;
-
---
--- Name: order_product_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.order_product_id_seq
     START WITH 1
@@ -241,20 +120,8 @@ CREATE SEQUENCE public.order_product_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.order_product_id_seq OWNER TO postgres;
-
---
--- Name: order_product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.order_product_id_seq OWNED BY public.order_product.id;
-
-
---
--- Name: order_table; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.order_table (
                                     order_id bigint NOT NULL,
@@ -263,13 +130,7 @@ CREATE TABLE public.order_table (
                                     user_id bigint NOT NULL,
                                     total_amount bigint DEFAULT 0 NOT NULL
 );
-
-
 ALTER TABLE public.order_table OWNER TO postgres;
-
---
--- Name: order_table_order_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.order_table_order_id_seq
     START WITH 1
@@ -277,20 +138,8 @@ CREATE SEQUENCE public.order_table_order_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.order_table_order_id_seq OWNER TO postgres;
-
---
--- Name: order_table_order_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.order_table_order_id_seq OWNED BY public.order_table.order_id;
-
-
---
--- Name: products; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.products (
                                  id bigint NOT NULL,
@@ -301,13 +150,7 @@ CREATE TABLE public.products (
                                  title character varying(255),
                                  user_id bigint
 );
-
-
 ALTER TABLE public.products OWNER TO postgres;
-
---
--- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.products_id_seq
     START WITH 1
@@ -315,36 +158,17 @@ CREATE SEQUENCE public.products_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.products_id_seq OWNER TO postgres;
-
---
--- Name: products_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.products_id_seq OWNED BY public.products.id;
-
-
---
--- Name: user_role; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.user_role (
                                   user_id bigint NOT NULL,
                                   roles character varying(255)
 );
-
-
 ALTER TABLE public.user_role OWNER TO postgres;
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.users (
                               id bigint NOT NULL,
-                              activation_code character varying(255),
                               active boolean NOT NULL,
                               email character varying(255),
                               name character varying(255),
@@ -352,13 +176,7 @@ CREATE TABLE public.users (
                               phone_number character varying(255),
                               avatar_id bigint
 );
-
-
 ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE public.users_id_seq
     START WITH 1
@@ -366,316 +184,89 @@ CREATE SEQUENCE public.users_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
-
---
--- Name: cart_item id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.cart_item ALTER COLUMN id SET DEFAULT nextval('public.cart_item_id_seq'::regclass);
-
-
---
--- Name: carts id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.carts ALTER COLUMN id SET DEFAULT nextval('public.carts_id_seq'::regclass);
-
-
---
--- Name: chat chat_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.chat ALTER COLUMN chat_id SET DEFAULT nextval('public.chat_chat_id_seq'::regclass);
-
-
---
--- Name: images id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.images ALTER COLUMN id SET DEFAULT nextval('public.images_id_seq'::regclass);
-
-
---
--- Name: message message_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.message ALTER COLUMN message_id SET DEFAULT nextval('public.message_message_id_seq'::regclass);
-
-
---
--- Name: order_product id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_product ALTER COLUMN id SET DEFAULT nextval('public.order_product_id_seq'::regclass);
-
-
---
--- Name: order_table order_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_table ALTER COLUMN order_id SET DEFAULT nextval('public.order_table_order_id_seq'::regclass);
-
-
---
--- Name: products id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.products_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- Name: cart_item cart_item_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.cart_item
     ADD CONSTRAINT cart_item_pkey PRIMARY KEY (id);
-
-
---
--- Name: carts carts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.carts
     ADD CONSTRAINT carts_pkey PRIMARY KEY (id);
-
-
---
--- Name: chat chat_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.chat
     ADD CONSTRAINT chat_pkey PRIMARY KEY (chat_id);
-
-
---
--- Name: chat chat_user_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.chat
     ADD CONSTRAINT chat_user_id_key UNIQUE (user_id);
-
-
---
--- Name: images images_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.images
     ADD CONSTRAINT images_pkey PRIMARY KEY (id);
-
-
---
--- Name: message message_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.message
     ADD CONSTRAINT message_pkey PRIMARY KEY (message_id);
-
-
---
--- Name: order_product order_product_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_product
     ADD CONSTRAINT order_product_pkey PRIMARY KEY (id);
-
-
---
--- Name: order_table order_table_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_table
     ADD CONSTRAINT order_table_pkey PRIMARY KEY (order_id);
-
-
---
--- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.products
     ADD CONSTRAINT products_pkey PRIMARY KEY (id);
-
-
---
--- Name: carts uk_64t7ox312pqal3p7fg9o503c2; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.carts
     ADD CONSTRAINT uk_64t7ox312pqal3p7fg9o503c2 UNIQUE (user_id);
-
-
---
--- Name: users uk_6dotkott2kjsp8vw4d0m25fb7; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT uk_6dotkott2kjsp8vw4d0m25fb7 UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
-
---
--- Name: users fk2lttmx8vn9eecykig5sch3v0h; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT fk2lttmx8vn9eecykig5sch3v0h FOREIGN KEY (avatar_id) REFERENCES public.images(id);
-
-
---
--- Name: chat fk_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.chat
     ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: carts fkb5o626f86h46m4s7ms6ginnop; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.carts
     ADD CONSTRAINT fkb5o626f86h46m4s7ms6ginnop FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: products fkdb050tk37qryv15hd932626th; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.products
     ADD CONSTRAINT fkdb050tk37qryv15hd932626th FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: order_table fkfc3ru564ptuisgtaklrqcw6r7; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_table
     ADD CONSTRAINT fkfc3ru564ptuisgtaklrqcw6r7 FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: images fkghwsjbjo7mg3iufxruvq6iu3q; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.images
     ADD CONSTRAINT fkghwsjbjo7mg3iufxruvq6iu3q FOREIGN KEY (product_id) REFERENCES public.products(id);
-
-
---
--- Name: user_role fkj345gk1bovqvfame88rcx7yyx; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.user_role
     ADD CONSTRAINT fkj345gk1bovqvfame88rcx7yyx FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: cart_item fklqwuo55w1gm4779xcu3t4wnrd; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.cart_item
     ADD CONSTRAINT fklqwuo55w1gm4779xcu3t4wnrd FOREIGN KEY (cart_id) REFERENCES public.carts(id);
-
-
---
--- Name: message fkmejd0ykokrbuekwwgd5a5xt8a; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.message
     ADD CONSTRAINT fkmejd0ykokrbuekwwgd5a5xt8a FOREIGN KEY (chat_id) REFERENCES public.chat(chat_id);
-
-
---
--- Name: order_product fko6helt0ucmegaeachjpx40xhe; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_product
     ADD CONSTRAINT fko6helt0ucmegaeachjpx40xhe FOREIGN KEY (product_id) REFERENCES public.products(id);
-
-
---
--- Name: order_product fkp9qp59hyy3jry2n6c0ikfmq7b; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.order_product
     ADD CONSTRAINT fkp9qp59hyy3jry2n6c0ikfmq7b FOREIGN KEY (order_id) REFERENCES public.order_table(order_id);
-
-
---
--- Name: message fkpdrb79dg3bgym7pydlf9k3p1n; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.message
     ADD CONSTRAINT fkpdrb79dg3bgym7pydlf9k3p1n FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: cart_item fkqkqmvkmbtiaqn2nfqf25ymfs2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.cart_item
     ADD CONSTRAINT fkqkqmvkmbtiaqn2nfqf25ymfs2 FOREIGN KEY (product_id) REFERENCES public.products(id);
 
-
---
--- Name: users fkqw6uf73w74vgfp4fpjf2jp6w0; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT fkqw6uf73w74vgfp4fpjf2jp6w0 FOREIGN KEY (chat_id) REFERENCES public.chat(chat_id);
-
-
---
--- PostgreSQL database dump complete
---
-
--- Вставка пользователя admin
-INSERT INTO users (
+INSERT INTO public.users (
     id,
-    activation_code,
     active,
     email,
     name,
     password,
     phone_number,
-    avatar_id,
-    chat_id
+    avatar_id
 ) VALUES (
              1,
-             NULL,
              true,
              'admin@mail.ru',
              'admin',
              '$2a$08$73B07faPCdygCB5q7FC2qO0tobjvm7pcqIT3fjPd6TGm4sExsQ1yG',
-             '+78977381210',
-             NULL,
+             '+79258802626',
              NULL
          );
 
--- Назначение ролей пользователю admin
-INSERT INTO user_role (user_id, roles) VALUES
+INSERT INTO public.user_role (user_id, roles) VALUES
                                            (1, 'ROLE_ADMIN'),
                                            (1, 'ROLE_USER');
